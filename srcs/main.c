9a0346d8261aff077d10e0c0126b0c049997a9cd
Luca BENARD <lubenard@student.42.fr>
2018-12-12 16:10:29 +0100

    Modif du main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jmoussu <jmoussu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 18:40:05 by lubenard          #+#    #+#             */
-/*   Updated: 2018/12/12 15:18:58 by jmoussu          ###   ########.fr       */
+/*   Updated: 2018/12/12 16:10:03 by lubenard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,8 @@ char	*read_file(char *arg)
 		ft_putstr("open fail\n");
 		return (NULL);
 	}
-	read(fd, str, BUFF);
+	if (read(fd, str, BUFF) < 0)
+		return (NULL);
 	if (close(fd) == -1)
 	{
 		ft_putstr("Close Fail.\n");
@@ -40,8 +41,6 @@ int		check_params(char argc, char **argv)
 {
 	if (argc == 1 || argc > 2)
 		return (usage());
-	if (!(read_file(argv[1])))
-		return (error());
 	return (1);
 }
 
@@ -52,15 +51,15 @@ int		main(int argc, char **argv)
 		return (-1);
 	if (!(str = read_file(argv[1])))
 		return (error());
-	if (valid_file(str) == -1)
-	{
-		ft_putstr("valid_file a renvoyer -1\n");
-		return(error());
-	}
-	else
-	{
-		ft_putstr("LE FICHIER EST VALIDE \nBIEN JOUER \n");
-	}
-		// parsing(read_file(argv[1]));
+	//if (valid_file(str) == -1)
+	//{
+	//	ft_putstr("valid_file a renvoyer -1\n");
+	//	return(error());
+	//}
+	//else
+	//{
+	//	ft_putstr("LE FICHIER EST VALIDE \nBIEN JOUER \n");
+	//}
+	parsing(read_file(argv[1]));
 	return (0);
 }
